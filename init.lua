9906f9a4e32c4b3130f7edc4ab9004e2ce77b0a7
Juan Felipe Agudelo <juan@192.168.80.21>
2025-04-12 18:42:34 -0500

    Initial commit: NVChad configuration setup

---
+++ init.lua
@@ -0,0 +1,39 @@
+vim.g.base46_cache = vim.fn.stdpath("data") .. "/base46/"
+vim.g.mapleader = " "
+
+-- Bootstrap lazy.nvim and all plugins
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.uv.fs_stat(lazypath) then
+  local repo = "https://github.com/folke/lazy.nvim.git"
+  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local lazy_config = require "configs.lazy"
+
+-- Load plugins using lazy.nvim:
+require("lazy").setup({
+  {
+    "NvChad/NvChad",
+    lazy = false,
+    branch = "v2.5",
+    import = "nvchad.plugins",  -- Load NvChad's own plugins
+  },
+
+  { import = "plugins" },        -- Load plugins from your default plugins folder
+  { import = "plugins.extra" },  -- Load your extra plugins from a custom file
+}, lazy_config)
+
+-- Load theme and statusline setups from the base46 cache
+dofile(vim.g.base46_cache .. "defaults")
+dofile(vim.g.base46_cache .. "statusline")
+
+require "options"
+require "nvchad.autocmds"
+
+vim.schedule(function()
+  require "mappings"
+end)
+

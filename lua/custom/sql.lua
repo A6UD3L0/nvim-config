5fba07ca8b1593d3a2cc02cb561d9f96ffde4043
A6UD3L0 <juanf.agudelor@gmail.com>
2025-04-21 07:24:53 -0500

    feat(options): improve comfort settings for long coding sessions (scrolloff, sidescrolloff, etc)

--- lua/custom/sql.lua
+++
@@ -1,6 +0,0 @@
--- sql.lua: Configure vim-dadbod and related plugins for SQL workflows
--- No <leader> mappings here; all are in keymaps.lua
--- UI/DB plugin config can be extended here if needed
-
-vim.g.dadbod_ui_auto_execute_table_helpers = 1
-vim.g.db_ui_use_nerd_fonts = 1
